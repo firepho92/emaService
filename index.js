11e98b45e778c74c58966b8305e4fb13dc3de71a
Alex Aguilar <auza920205@gmail.com>
2020-09-13 14:28:53 -0500

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const SerialPort = require('serialport')
+const readline = require('@serialport/parser-readline')
+const usbPort = new SerialPort('/dev/ttyACM0', { baudRate: 115200 })
 const app = express()
 const port = 8000
 
