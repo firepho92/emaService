1e834683967eeaab235038725ffdc0e351bd190f
Alex Aguilar <auza920205@gmail.com>
2020-09-13 14:30:04 -0500

    ok root route added

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const { Router } = require('express')
 
 const router = Router()
 
+router.get('/', (req, res) => res.status(200).send('ok'))
+
 router.get('/data', async (req, res) => res.send('aló'))
 
 router.post('/administration', async(req, res) => send('aló'))
