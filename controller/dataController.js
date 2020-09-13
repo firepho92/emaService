a5ba5c609afdd5d7d5c50f8139e06414d0a4e251
Alex Aguilar <auza920205@gmail.com>
2020-09-13 15:42:57 -0500

    data parsed and date added

--- controller/dataController.js
+++ controller/dataController.js
@@ -1,15 +1,36 @@
 const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
+const moment = require('moment')
 const usbPort = new SerialPort('/dev/ttyACM0', { baudRate: 115200 })
 
 const getData = async (req, res) => {
-  try { 
+  moment.locale('es')
+  const time = moment().format('LT')
+  const date = moment().format('L')
+  console.log(time + '_' + date)
+  try {
     const parser = new Readline()
     usbPort.pipe(parser)
     usbPort.write('hola \n')
     parser.on('data', (data) => {
+      console.log(data)
+      let json = JSON.parse(data)
+      json = {
+        date: Date.now(),
+        temperature: Math.round((json.temperature + Number.EPSILON) * 100) / 100,
+        humidity: Math.round((json.humidity + Number.EPSILON) * 100) / 100,
+        pressure: Math.round((json.pressure + Number.EPSILON) * 100) / 100,
+        windSpeed: Math.round((json.windSpeed + Number.EPSILON) * 100) / 100,
+        windDirection: json.windDirection,
+        location: {
+          latitude: json.latitude,
+          longitude: json.longitude,
+          altitude: json.altitude
+        }
+      }
+      console.log(json)
       parser.end()
-      res.status(200).send(data)
+      res.status(200).send(json)
     })
 
   } catch(e) {
