08a0f3463421e9515bf06c49abeab69b90eefe60
Alex Aguilar <auza920205@gmail.com>
2020-09-13 17:43:38 -0500

    morgan added

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const routes = require('./routes')
 const app = express()
 const port = 8000
 
+app.use(morgan('combined'))
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
